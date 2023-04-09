5b0c2d4844d3ea1be888ce13931321769052ae31
Jihad Haddou <101198192+webenfly1@users.noreply.github.com>
2023-04-09 23:43:33 +0000

    🧪 Add Jest TDD tests for API route to ensure functionality and reliability

--- next.config.js
+++ next.config.js
@@ -2,24 +2,4 @@
 const nextConfig = {
   reactStrictMode: true,
 }
-
-//module.exports = nextConfig
-
-
-module.exports = {
-  future: {
-    webpack5: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = { fs: false, module: false }
-    }
-    return config
-  },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-}
+module.exports = nextConfig
