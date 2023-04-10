fb5ba020e3741a7a449c1545bfc432ea90d173ed
Jihad Haddou <101198192+webenfly1@users.noreply.github.com>
2023-04-10 19:36:09 +0000

    Optimize API route for Vercel deployment

--- src/pages/api/stock/[company].ts
+++ src/pages/api/stock/[company].ts
@@ -9,8 +9,10 @@ export default async function handler(
   if (company !== "GOOGLE" && company !== "AMAZON") {
     return res.status(404).end();
   }
-  const client = await MongoClient.connect(process.env.MONGODB_URI as string);
+  const client = await MongoClient.connect("mongodb+srv://haddoujihad:kWiSXQjvStwl3wTS@cluster0.vsgein5.mongodb.net/?retryWrites=true&w=majority");
   const db = client.db("stock-data").collection("prices");
-  const data = await db.find({ company }).toArray();
+  //const data = await db.find({ company }).toArray();
+  await db.createIndex({ company: 1 });
+  const data = await db.find({ company }).limit(5).toArray();
   res.json(data);
 }
