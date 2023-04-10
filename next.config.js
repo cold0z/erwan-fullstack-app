24d253f2beed0607b228a8cf5c118a3cabd2447c
Jihad Haddou <101198192+webenfly1@users.noreply.github.com>
2023-04-10 00:00:47 +0000

    🚀 Implemented a feature to help Aymen and Anouar optimize their investment strategy.

--- next.config.js
+++ next.config.js
@@ -1,25 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-//module.exports = nextConfig
-
-
-module.exports = {
-  future: {
-    webpack5: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = { fs: false, module: false }
-    }
-    return config
-  },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-}
+module.exports = nextConfig;
